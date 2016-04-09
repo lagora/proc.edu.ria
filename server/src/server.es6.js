91646b87d7b8d58016658f06b753f3a68f9bd832
Franck Lagora <lagora@users.noreply.github.com>
2016-04-10 00:19:19 +0200

    Update server.es6.js

--- server/src/server.es6.js
+++ server/src/server.es6.js
@@ -1,17 +1,1 @@
-import generator from './generator.es6.js'
-var server = ws.listen(8081)
-import ProceduriaBuilder from './ProceduriaBuilder.es6.js'
 
-server.on('upgrade', function (req, socket, head) {
-  server.handleUpgrade(req, socket, head);
-});
-
-server.on('connection', function (client) {
-  client.on('ready', function () {
-    console.log('client is ready')
-    var builder = new ProceduriaBuilder();
-    builder.make();
-  });
-  client.on('message', function () { });
-  client.on('close', function () { });
-});
