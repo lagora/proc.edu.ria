f8fabcba31f6772a453986b83f03bda1d0066d7c
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-04-16 16:34:21 +0200

    server now do lots of checking

--- server/src/server.es6.js
+++ server/src/server.es6.js
@@ -1,69 +1,97 @@
+import waterfall from 'async-waterfall'
+import fs  from 'fs-extra'
 import connect from 'connect'
-var app = connect();
 import serveStatic from 'serve-static'
 import http from 'http'
 // var server = http.Server(app);
 
 import bodyParser from 'connect-hopeful-body-parser'
 import generator from '../../src/generator.es6.js'
-
 var Datastore = require('nedb');
-var db = new Datastore()
-
-app
-.use(bodyParser())
-// .use('/cfg', (req, res, next) => {
-//   generator(req.body, (err, results) => {
-//     res.end()
-//   })
-// })
-.use(
-  serveStatic("./")
-).listen(1337, () => {
-  console.info('Proc.Edu.Ria')
-});
-
 var WebSocketServer = require("websocketserver");
-var server = new WebSocketServer("one", 8015);
 
-server.webSocketOut = (objectData, id) => {
-  // let packagedMessage = server.packageMessage(id, JSON.stringify(objectData));
-  server.sendMessage('one', JSON.stringify(objectData), id);
-}
-
-server.on("connection", (id) => {
-  console.log('connection', id);
-});
-
-
-server.on("message", (rawData, id) => {
-  let mesObj = server.unmaskMessage(rawData);
-  let unmaskedData = server.convertToString(mesObj.message);
-  let jsonMessage = JSON.parse(unmaskedData);
-  let event = jsonMessage.event;
-  let data = jsonMessage[event];
-  switch (event) {
-    default:
-    // let packagedMessage = server.packageMessage(mesObj.opcode, JSON.stringify(item));
-    // server.sendMessage('one', packagedMessage);
-    break;
-    case 'cfg':
-      data.ws = server;
-      data.wsId = id;
-      data.db = db;
-      generator(data);
-    break;
-    case 'raw':
-      db.find({type: 'raw'}, (err, items) => {
-        console.log('err', err, 'items', items);
-        if (err) {
-          server.webSocketOut(err, id);
-        } else if (!items || 0 === items.length) {
-          server.webSocketOut({statusCode: 404, message: "no items"}, id);
-        } else {
-          server.webSocketOut(items, id);
-        }
-      })
-    break;
+waterfall([
+  (next) => {
+    let rulesIndexFilename = './rules.json';
+    let rulesIndexFound = fs.existsSync(rulesIndexFilename);
+    let err = rulesIndexFound ? null:new Error('rules index not found');
+    let rulesIndex = rulesIndexFound ? fs.readJsonSync(rulesIndexFilename):null;
+    next(err, rulesIndex);
+  },
+  (rulesIndex, next) => {
+    let cfg = rulesIndex;
+    next(null, cfg);
+  },
+  (cfg, next) => {
+    let oneRuleFileNotFound = cfg.files.some((fileData) => !fs.existsSync(`./${cfg.path}/${fileData.filename}`));
+    let err = oneRuleFileNotFound ? new Error(`one rule file is missing in ${cfg.path}`):null
+    next(err, cfg);
+  },
+  (cfg, next) => {
+    cfg.rules = cfg.files.map((fileData) => fs.readJsonSync(`./${cfg.path}/${fileData.filename}`));
+    next(null, cfg);
+  },
+  (cfg, next) => {
+    cfg.db = new Datastore();
+    next(null, cfg);
   }
-})
+], (err, cfg) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  let serverPort = 1337;
+  let websocketPort = 8015;
+  var app = connect();
+  var server = new WebSocketServer("one", websocketPort);
+
+  app
+  // .use(bodyParser())
+  .use(
+    serveStatic("./")
+  ).listen(serverPort, () => {
+    console.info(`Proc.Edu.Ria server listening on port ${serverPort}`)
+  });
+  server.webSocketOut = (objectData, id) => {
+    server.sendMessage('one', JSON.stringify(objectData), id);
+  }
+  server.on("connection", (id) => {
+    console.log('connection', id);
+  });
+
+  server.on("message", (rawData, id) => {
+    let mesObj = server.unmaskMessage(rawData);
+    let unmaskedData = server.convertToString(mesObj.message);
+    let jsonMessage = JSON.parse(unmaskedData);
+    console.log('message', id, jsonMessage);
+    let event = jsonMessage.event;
+    let data = jsonMessage[event];
+    switch (event) {
+      default:
+      console.log('unhandled request', rawData);
+      break;
+      case 'cfg':
+        data.ws = server;
+        data.wsId = id;
+        data.db = cfg.db;
+        data.rules = cfg.rules;
+        generator(data);
+      break;
+      case 'raw':
+        console.info('looking for previous raw data');
+        db.find({type: 'raw'}, (err, items) => {
+          console.log('err', err, 'items', items);
+          if (err) {
+            server.webSocketOut(err, id);
+          } else if (!items || 0 === items.length) {
+            server.webSocketOut({statusCode: 404, message: "no items"}, id);
+          } else {
+            server.webSocketOut(items, id);
+          }
+        })
+      break;
+    }
+  })
+
+});
