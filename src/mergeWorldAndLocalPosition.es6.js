511a492218e161f9c960453c54249843a68168d8
Franck Lagora <lagora@users.noreply.github.com>
2016-05-10 18:19:17 +0200

    Update mergeWorldAndLocalPosition.es6.js

--- src/mergeWorldAndLocalPosition.es6.js
+++ src/mergeWorldAndLocalPosition.es6.js
@@ -1,6 +1,6 @@
 export default function(worldPosition, localPosition) {
   let merged = {};
-  ['x', 'y', 'z'].forEach((axis) => {
+  ["x", "y", "z"].forEach((axis) => {
     merged[axis] = worldPosition[axis] + localPosition[axis];
   });
   return merged;
