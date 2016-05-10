ae71ce4ede566c55366fe863413442abb2d0c3eb
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-11 01:57:28 +0200

    cleaning

--- src/generator.es6.js
+++ src/generator.es6.js
@@ -1,16 +1,11 @@
-import getCacheName from './getCacheName.es6.js';
-import * as r from "./rules.es6.js";
 
 function generator (cfg, callback) {
   let cacheName = getCacheName("generator", cfg);
   let data = window.localStorage.getItem(cacheName);
-  console.trace(`using cache: ${!!data}`);
-
   if (!data) {
     data = Object.values(r).map((rule) => {
       return rule(cfg);
     });
-    console.trace("setting up cache");
     window.localStorage.setItem(cacheName, JSON.stringify(data));
   } else {
     try {
