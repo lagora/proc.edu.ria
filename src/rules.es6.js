7e93be18547f06169d1a5d9e4596e3444228a196
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-10 19:04:32 +0200

    adding rules, dist, and build to npm test cmd

--- src/rules.es6.js
+++ src/rules.es6.js
@@ -1,4 +1,4 @@
-import rule_0 from './rules/rule.0.es6.js';
-import rule_1 from './rules/rule.1.es6.js';
+import rule_0 from "./rules/rule.0.es6.js";
+import rule_1 from "./rules/rule.1.es6.js";
 
 export { rule_0, rule_1 };
