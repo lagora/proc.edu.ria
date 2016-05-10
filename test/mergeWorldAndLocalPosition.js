0c8378279542c3c2c043126d63686a49227ceb84
Franck Lagora <lagora@users.noreply.github.com>
2016-05-10 17:52:54 +0200

    Create mergeWorldAndLocalPosition.js

---
+++ test/mergeWorldAndLocalPosition.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill';
+import mergePositions from '../src/mergeWorldAndLocalPosition.es6.js';
+import assert from 'assert';
+
+describe("mergeWorldAndLocalPosition.js", () => {
+  it("is a function", () => {
+    assert(typeof position === 'function');
+  });
+  it("merge and return the world position and local position givent as arguments", () => {
+    let result = mergePositions({ x:1, y:2, z:3 }, { x: 4, y:5, z:6 });
+    assert('object' === typeof result);
+    assert(result.x === 5);
+    assert(!isNaN(result.x));
+    assert(result.y === 7);
+    assert(!isNaN(result.y));
+    assert(result.z === 9);
+    assert(!isNaN(result.z));
+  });
+});
