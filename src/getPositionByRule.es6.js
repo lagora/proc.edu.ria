3f32cb654fef533c9729c5488228158856da3724
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-11 01:57:46 +0200

    cleaning

--- src/getPositionByRule.es6.js
+++ src/getPositionByRule.es6.js
@@ -1,7 +1,6 @@
 import mergeWorldAndLocalPosition from "./mergeWorldAndLocalPosition.es6.js";
 import adjustPositionBySize from "./adjustPositionBySize.es6.js";
 import getLocalPositionByRule from "./getLocalPositionByRule.es6.js";
-import getRuleMaterialOptions from "./getRuleMaterialOptions.es6.js";
 
 function getPositionByRule(ruleData, bit, size) {
   let rawLocalPosition = getLocalPositionByRule(ruleData);
