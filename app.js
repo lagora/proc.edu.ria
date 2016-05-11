8bc3c407e746b40e14b3019bd0eb4290f21a81e1
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-11 02:06:36 +0200

    cleaning

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // simple express server
 var express = require("express");
 var app = express();
-var router = express.Router();
 
 app.use(express.static("./dist/"));
 app.get("/", function(req, res) {
