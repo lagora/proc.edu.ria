f74ba6749896c15aef3cf98bfbb81f0fe18cd14e
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-11 02:22:15 +0200

    cleaning

--- test/scan.js
+++ test/scan.js
@@ -1,10 +1,8 @@
 import "babel-polyfill";
 import scan from "../src/scan.es6.js";
 import assert from "assert";
-import jsdom from "mocha-jsdom";
 
 describe("scan.js", () => {
-  jsdom();
   it("is a function", (done) => {
     assert(typeof scan === "function");
     done();
