1df6059d5d9d2d337381fa5646ff108bb1edb102
Franck Lagora <lagora@users.noreply.github.com>
2016-05-11 14:15:06 +0200

    Update getHelpers.es6.js

--- src/getHelpers.es6.js
+++ src/getHelpers.es6.js
@@ -1,11 +1,6 @@
 import THREE from "three";
-import cfg from "./config.es6.js";
 
-function getHelpers(cfg) {
-  if (!cfg.debug) {
-    return {};
-  }
-
+function getHelpers() {
   var gridHelper = new THREE.GridHelper( cfg.size - 1 , 1 );
   // gridHelper.position.set( cfg.size / 2, cfg.size / 2);
   gridHelper.position.x = cfg.size / 2;
