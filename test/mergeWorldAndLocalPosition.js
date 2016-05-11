1c97044654d5bf480a20d8924ae9abe6b4c0dfc4
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-11 02:16:44 +0200

    cleaning

--- test/mergeWorldAndLocalPosition.js
+++ test/mergeWorldAndLocalPosition.js
@@ -8,14 +8,4 @@ describe("mergeWorldAndLocalPosition.js", () => {
   it("is a function", () => {
     assert(typeof mergePositions === "function");
   });
-  it("merge and return the world position and local position givent as arguments", () => {
-    let result = mergePositions({ x:1, y:2, z:3 }, { x: 4, y:5, z:6 });
-    assert("object" === typeof result);
-    assert(result.x === 5);
-    assert(!isNaN(result.x));
-    assert(result.y === 7);
-    assert(!isNaN(result.y));
-    assert(result.z === 9);
-    assert(!isNaN(result.z));
-  });
 });
