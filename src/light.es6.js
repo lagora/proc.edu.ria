b3bf2ede489d9d50bd0952b83c498bbdbca05f40
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-12 13:46:10 +0200

    localStorage maxSize check, better light

--- src/light.es6.js
+++ src/light.es6.js
@@ -5,10 +5,10 @@ var HemisphereLight = new THREE.HemisphereLight( 0x777777, 0xcccccc, 0.5 );
 var DirectionalLight = new THREE.DirectionalLight( 0xffffbb, 0.5 );
 DirectionalLight.position.set( 0, 1, 0.75 );
 var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 10, 100, 10 );
+spotLight.position.set( 10, cfg.size * 2, cfg.size );
 
 spotLight.castShadow = true;
-let shadowMapSize = 2048;
+let shadowMapSize = 4096;
 spotLight.shadow.mapSize.width = shadowMapSize;
 spotLight.shadow.mapSize.height = shadowMapSize;
 
