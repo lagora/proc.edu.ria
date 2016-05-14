d9efb5a8222782e67d039da46a75ae6208ccc49f
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-14 16:19:46 +0200

    wip

--- src/init.es6.js
+++ src/init.es6.js
@@ -1,6 +1,6 @@
 import THREE from "three";
 import cfg from "./config.es6.js";
-import * as lights from "./light.es6.js";
+import getLights from "./getLights.es6.js";
 import getHelpers from "./getHelpers.es6.js";
 import getLightHelper from "./getLightHelper.es6.js";
 import sceneAdd from "./sceneAdd.es6.js";
@@ -9,6 +9,8 @@ var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 scene.up = new THREE.Vector3(0, 0, 1);
+
+var lights = getLights(cfg);
 
 Object.keys(lights).forEach((lightType) => {
   sceneAdd(scene, getLightHelper(cfg, lightType, lights[lightType]));
