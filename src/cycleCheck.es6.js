f669d89aca08dd35b8e81a21d14744b5faae987d
Franck Lagora <lagora@users.noreply.github.com>
2016-05-14 14:38:51 +0200

    Update cycleCheck.es6.js

--- src/cycleCheck.es6.js
+++ src/cycleCheck.es6.js
@@ -1,7 +1,7 @@
 function cycleCheck (list, keys, all = false) {
   var err = false;
   keys[ all ? "map":"some"]((key) => {
-    if (typeof list[key] === undefined) {
+    if (list[key] === undefined || typeof list[key] === 'undefined') {
       err = `missing ${key}`;
     }
   });
