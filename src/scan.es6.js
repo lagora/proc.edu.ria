f5c3f07f1564cb6790c97fdef42779dd1fecf619
François Lunette <Francois.LUNETTE@radiofrance.com>
2016-05-14 15:14:53 +0200

    fix

--- src/scan.es6.js
+++ src/scan.es6.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-function scan(max, step = 1) {
-  let i = 0;
-  let data = [];
-  for (let x = 0; x < max; x += step) {
-    for (let y = 0; y < max; y += step) {
-      for (let z = 0; z < max; z += step) {
-        // yield { i, x, y, z };
-        data.push({ i, x, y, z });
-        i++;
-      }
-    }
-  }
-
-  return data;
-=======
 // import getCacheName from "./getCacheName.es6.js";
 // import cfg from "./config.es6.js";
 
@@ -36,7 +20,6 @@ function scan(max, step = 1) {
   // }
 
   return cached;
->>>>>>> b4cec013a9cabade53f444424891dab010123043
 }
 
 export default scan;
