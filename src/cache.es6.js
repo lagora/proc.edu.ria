06e33f15aa622ef01c8a166ca76b1c2ac9a0dc56
Franck Lagora <lagora@users.noreply.github.com>
2016-05-14 14:34:15 +0200

    Update cache.es6.js

--- src/cache.es6.js
+++ src/cache.es6.js
@@ -1,12 +1,13 @@
 // import getCacheName from "./getCacheName.es6.js";
-
+// WIP: not yet usable
 var cache = {
   get: () => {
     console.trace("arguments", arguments);
     // window.localStorage.getItem(key);
   },
   set: (value) => {
-
+    let name = "temp-name";
+    window.localStorage.setItem(name, value);
   }
 }
 
